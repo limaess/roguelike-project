79355477bc6ff1cdf064a13bf89b5d5083b91a8a
limaes <gitmaes@gmail.com>
2026-03-14 23:01:48 +0800

    inventory: made the class

--- scripts/player_inventory.py
+++ scripts/player_inventory.py
@@ -2,5 +2,8 @@ import pygame as pg
 
 inventory = []
 class Inventory():
-    def __init__(self, max_amt):
-        pass
+    def __init__(self, max_amt, inventory_value):
+        self.max_amt = max_amt
+        
+        self.inventory_value = inventory_value
+
