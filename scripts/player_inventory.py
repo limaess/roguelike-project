b63bf1a5e3bbff16ba957541a4d8194bd4d4d14a
limaes <gitmaes@gmail.com>
2026-03-15 02:29:35 +0800

    inventory: added spell and passive classes, added lists for them

--- scripts/player_inventory.py
+++ scripts/player_inventory.py
@@ -1,8 +1,14 @@
 import pygame as pg
 
-inventory = []
+consumable_inventory = []
+spell_inventory = []
+passive_inventory = []
 
-class Consumables:
+all_consumables = []
+all_spells = []
+all_passives = []
+
+class Consumable:
     def __init__(self, description,name,amount,value, item_func, uses_left): # amount - for using|value - for selling or whatever
         self.name = name
         self.description = description
@@ -14,27 +20,50 @@ class Consumables:
         self.item_func = item_func
         self.uses_left = uses_left
 
+class Passive:
+    def __init__(self, description,name,amount,item_func, uses_left):
+        self.name = name
+        self.description = description
+
+        self.amount = amount
+
+        self.item_func = item_func
+        self.uses_left = uses_left
+
+class Spell:
+    def __init__(self, description,name,amount,item_func, uses_left):
+        self.name = name
+        self.description = description
+
+        self.amount = amount
+
+        self.item_func = item_func
+        self.uses_left = uses_left
+
+
 class Inventory:
-    def __init__(self, max_amt,amt_inside, current_amt_inside, inventory_value):
+    def __init__(self, max_amt,amt_inside, current_amt_inside, inventory, inventory_value):
         self.max_amt = max_amt
         self.amt_inside = amt_inside
         self.current_amt_inside = current_amt_inside
+
+        self.inventory = inventory
 
         self.inventory_value = inventory_value
         self.value_calculated = False
 
     def addItem(self, item):
         if not self.amt_inside >= self.max_amt:
-            inventory.append(item)
+            self.inventory.append(item)
         else:
             print('no')
     def deleteItem(self, item):
-        inventory.remove(item)
+        self.inventory.remove(item)
 
-    def calculateInvValue(self):
-        if inventory and not self.value_calculated: # if there is anything in inventory and value wasnt calculated:
-            for item in inventory:
-                item.value += self.inventory_value # item value adds up to inventory value for every item in inv
+    def calculateInvValue(self, item):
+        if self.inventory and not self.value_calculated and item in all_consumables: # if there is anything in inventory and value wasnt calculated:
+            for x in self.inventory:
+                x.value += self.inventory_value # item value adds up to inventory value for every item in inv
                 self.current_amt_inside += 1
                 self.amt_inside = self.current_amt_inside # genuinely dont know why this is here
             self.value_calculated = True
@@ -43,4 +72,6 @@ class Inventory:
                 self.inventory_value = 0 # it does this
                 self.value_calculated = False
 
-class_inventory = Inventory(max_amt=6,amt_inside=0,current_amt_inside=0,inventory_value=0)
+consum_inv = Inventory(6, 0,0, consumable_inventory, 0)
+spell_inv = Inventory(5, 0,0, spell_inventory, 0)
+passive_inv = Inventory(10, 0,0, passive_inventory, 0)
