15930e20e4c1b9a3abbcbb66fb56daf4872b09bf
limaes <gitmaes@gmail.com>
2026-03-14 22:21:50 +0800

    enemy: basic attack func

--- scripts/enemy_class.py
+++ scripts/enemy_class.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 class Enemy:
-    def __init__(self,constitution, health,speed, strength,dexterity,magic, classWeaknesses, howWeak):
+    def __init__(self,constitution, health,speed, strength,dexterity,magic, selfClassDamage, classWeaknesses, howWeak):
         self.constitution = constitution
 
         self.health = health
@@ -13,5 +13,15 @@ class Enemy:
 
         self.maxHealth = constitution * 4
 
+        self.selfClassDamage = selfClassDamage
         self.classWeaknesses = classWeaknesses
         self.howWeak = howWeak
+
+    def basicAttack(self, target): 
+        if not self.hasAttacked:
+            if not self.classWeaknesses == target.playerClass:
+                target.health -= self.yourClassDamage 
+            else:
+                target.health -= (self.yourClassDamage - self.howWeak)
+
+        self.hasAttacked = True
