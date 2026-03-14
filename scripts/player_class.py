1ec79a19f4c49bf2d37e45a1cef932c8ad874cf4
limaes <gitmaes@gmail.com>
2026-03-14 22:04:22 +0800

    player: added basic attack func

--- scripts/player_class.py
+++ scripts/player_class.py
@@ -1,16 +1,26 @@
 import pygame as pg
 
 class Player():
-    def __init__(self,constitution, health,damage,speed, playerClass, effectOnSelf):
+    def __init__(self,constitution, health,speed,mana, strength,dexterity,magic, intelligence,charisma, lifesteal, playerClass, yourClassDamage, effectOnSelf, whichSpellUsed):
         self.constitution = constitution
         self.health = health
-        self.damage = damage
         self.speed = speed
+        self.mana = mana
+
+        self.strength = strength
+        self.dexterity = dexterity
+        self.magic = magic
+
+        self.intelligence = intelligence
+        self.charisma = charisma
+
+        self.lifesteal = lifesteal
 
         self.maxHealth = constitution * 4
 
-        self.isDamagingSomeone = False
+        self.hasAttacked = False
+        self.whichSpellUsed = whichSpellUsed
         
         self.playerClass = playerClass
-        
+        self.yourClassDamage = yourClassDamage
         self.effectOnSelf = effectOnSelf
