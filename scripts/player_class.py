e8661fe4b5e7375541eb408c52f0cbb94db5c8fe
limaes <gitmaes@gmail.com>
2026-03-14 22:43:33 +0800

    player: updated code to use_this for variables

--- scripts/player_class.py
+++ scripts/player_class.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 class Player():
-    def __init__(self,constitution, health,speed,mana, strength,dexterity,magic, intelligence,charisma, lifesteal, playerClass, yourClassDamage, effectOnSelf, whichSpellUsed):
+    def __init__(self,constitution, health,speed,mana, strength,dexterity,magic, intelligence,charisma, lifesteal, self_class, self_class_damage, effect_on_self, which_spell_used):
         self.constitution = constitution
         self.health = health
         self.speed = speed
@@ -18,26 +18,26 @@ class Player():
 
         self.maxHealth = constitution * 4
 
-        self.hasAttacked = False
-        self.whichSpellUsed = whichSpellUsed
+        self.has_attacked = False
+        self.whichSpellUsed = which_spell_used
         
-        self.playerClass = playerClass
-        self.yourClassDamage = yourClassDamage
-        self.effectOnSelf = effectOnSelf
+        self.self_class = self_class
+        self.self_class_damage = self_class_damage
+        self.effect_on_self = effect_on_self
 
     def basicAttack(self, target):
-        if not self.hasAttacked:
-            if not target.classWeaknesses == self.playerClass: 
-                target.health -= (self.damage - target.waveBuffs)
+        if not self.has_attacked:
+            if not target.class_weaknesses == self.self_class: 
+                target.health -= (self.damage - target.wave_buffs)
             else:
                 target.health -= (self.damage + target.howWeak)
         else:
             print('you already attacked, but this message wont appear later')
-        self.hasAttacked = True
+        self.has_attacked = True
 
     def limits(self):
-        if self.health >= self.maxHealth:
-            self.health = self.maxHealth
+        if self.health >= self.max_health:
+            self.health = self.max_health
         if self.health <= 0:
             self.health = 0
 
