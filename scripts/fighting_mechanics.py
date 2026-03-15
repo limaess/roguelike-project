1748023f8e9dcb396828cd1a846c0c2a3c293023
limaes <gitmaes@gmail.com>
2026-03-15 17:49:32 +0800

    fighting: end turn func

--- scripts/fighting_mechanics.py
+++ scripts/fighting_mechanics.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random as rnd
 
 class TurnSys:
     def __init__(self, whos_turn, which_round, exhaustion):
@@ -10,9 +11,23 @@ class TurnSys:
 
         self.turn_order = []
 
+    def endTurn(self,player,enemy, player_inv,enemy_inv):
+        self.whosTurn += 1
+        if self.whosTurn == 5:
+            self.whosTurn = 0
+
+        player.has_attacked = False
+        enemy.has_attacked = False
+
+        for item in player_inv:
+            item.been_used = False
+        for item in enemy_inv:
+            item.been_used = False
+
     def makeTurnOrder(self,player, enemy1,enemy2,enemy3):
         if not self.turn_order:
             self.turn_order = [player,enemy1,enemy2,enemy3,player]
+            return True
         else:
             print('why am i calling this function?')
 
