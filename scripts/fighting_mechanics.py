dcd4a9e8057d8b2db75ba566e95d0495144f208b
limaes <gitmaes@gmail.com>
2026-03-15 17:46:59 +0800

    fighting: im so dumb

--- scripts/fighting_mechanics.py
+++ scripts/fighting_mechanics.py
@@ -6,14 +6,14 @@ class TurnSys:
         self.which_round = which_round
         self.exhaustion = exhaustion
 
-        self.turnThingIdk = None
+        self.turn_thing = None
 
-        self.turnOrder = []
+        self.turn_order = []
 
     def makeTurnOrder(self,player, enemy1,enemy2,enemy3):
-        if not self.turnOrder:
-            self.turnOrder = [player,enemy1,enemy2,enemy3,player]
+        if not self.turn_order:
+            self.turn_order = [player,enemy1,enemy2,enemy3,player]
         else:
             print('why am i calling this function?')
 
-        self.turnThingIdk = self.turnOrder[self.whosTurn]
+        self.turn_thing = self.turn_order[self.whos_turn]
