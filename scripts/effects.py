4bbd02509d93f89ab1b17b61aa1f4f5456fc73bf
limaes <gitmaes@gmail.com>
2026-03-16 19:52:04 +0800

    effects: stunnedEffect func, added funcs tied to the elements in the effects dictionary

--- scripts/effects.py
+++ scripts/effects.py
@@ -6,39 +6,49 @@ from player_class import *
 from player_inventory import *
 
 class Effect:
-    def __init__(self):
-        
+    def __init__(self, effects_on_self: list, target, target_consum_inv, target_spell_inv):
+        self.target = target
+        self.target_consum_inv = target_consum_inv
+        self.target_spell_inv = target_spell_inv
+
+        self.effects_on_self = effects_on_self
+
         self.effects = {
-            'on_fire': {'active': False, 'applied': False},
-            'electric': {'active': False, 'applied': False},
-            'stunned': {'active': False, 'applied': False},
-            'leeched': {'active': False, 'applied': False},
-            'dmg_down': {'active': False, 'applied': False},
-            'const_down': {'active': False, 'applied': False},
-            'charisma_down': {'active': False, 'applied': False},
-            'intelligence_down': {'active': False, 'applied': False},
+            'on_fire': {'active': False, 'applied': False, 'func': {self.onFireEffect}},
+            'electric': {'active': False, 'applied': False, 'func': {self.electricEffect}},
+            'stunned': {'active': False, 'applied': False, 'func': {self.stunnedEffect}},
+            'leeched': {'active': False, 'applied': False, 'func': {self.leechedEffect}},
+            'dmg_down': {'active': False, 'applied': False, 'func': {self.dmgDown}},
+            'const_down': {'active': False, 'applied': False, 'func': {self.constDown}},
+            'charisma_down': {'active': False, 'applied': False, 'func': {self.charismaDown}},
+            'intelligence_down': {'active': False, 'applied': False, 'func': {self.intelligenceDown}},
         }
         
-    def onFireEffect(self, how_long, amount, applied):
+    def onFireEffect(self, how_long, amount):
         pass
 
-    def electricEffect(self, how_long, amount, applied):
+    def electricEffect(self, how_long, amount):
         pass
     
-    def stunnedEffect(self, how_long, amount, applied):
+    def stunnedEffect(self, how_long, amount):
+        if not how_long == 0: 
+            if self.target.has_attacked == False:
+                self.target.has_attacked = True 
+                how_long -= 1
+                for item in self.target_inventory.extend(self.target_spell_inv):
+                    item.been_used = False
+    
+    def leechedEffect(self, how_long, amount):
         pass
     
-    def leechedEffect(self, how_long, amount, applied):
+    def dmgDown(self, how_long, amount):
         pass
     
-    def dmgDown(self, how_long, amount, applied):
+    def constDown(self, how_long, amount):
         pass
     
-    def constDown(self, how_long, amount, applied):
-        pass
-    
-    def charismaDown(self, how_long, amount, applied):
+    def charismaDown(self, how_long, amount):
         pass
 
-    def intelligenceDown(self, how_long, amount, applied):
+    def intelligenceDown(self, how_long, amount):
         pass
